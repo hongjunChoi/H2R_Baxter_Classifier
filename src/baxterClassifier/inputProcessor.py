91f1ac78f773847bf0df09abbfd0e5479ccf9949
Hongjun Choi <hong_jun_choi@brown.edu>
2016-11-29 17:14:00 -0500

    sync

--- src/baxterClassifier/inputProcessor.py
+++ src/baxterClassifier/inputProcessor.py
@@ -70,32 +70,6 @@ def getTrainingInput():
     return train_image_batch, train_label_batch
 
 
-def read_my_file_format(filename_queue):
-    reader = tf.SomeReader()
-    key, record_string = reader.read(filename_queue)
-    example, label = tf.some_decoder(record_string)
-    processed_example = some_processing(example)
-    return processed_example, label
-
-
-def input_pipeline(filenames, batch_size, num_epochs=None):
-    filename_queue = tf.train.string_input_producer(
-        filenames, num_epochs=num_epochs, shuffle=True)
-    example, label = read_my_file_format(filename_queue)
-    # min_after_dequeue defines how big a buffer we will randomly sample
-    #   from -- bigger means better shuffling but slower start up and more
-    #   memory used.
-    # capacity must be larger than min_after_dequeue and the amount larger
-    #   determines the maximum we will prefetch.  Recommendation:
-    #   min_after_dequeue + (num_threads + a small safety margin) * batch_size
-    min_after_dequeue = 10000
-    capacity = min_after_dequeue + 3 * batch_size
-    example_batch, label_batch = tf.train.shuffle_batch(
-        [example, label], batch_size=batch_size, capacity=capacity,
-        min_after_dequeue=min_after_dequeue)
-    return example_batch, label_batch
-
-
 def inputs(eval_data, data_dir, batch_size):
     """Construct input for CIFAR evaluation using the Reader ops.
     Args:
@@ -195,3 +169,29 @@ def getBatchInput():
         min_after_dequeue=min_queue_examples)
 
     return images_batch
+
+
+def read_my_file_format(filename_and_label_tensor):
+    """Consumes a single filename and label as a ' '-delimited string.
+    Args:
+      filename_and_label_tensor: A scalar string tensor.
+
+    Returns:
+      Two tensors: the decoded image, and the string label.
+    """
+    filename, label = tf.decode_csv(
+        filename_and_label_tensor, [[""], [""]], " ")
+    file_contents = tf.read_file(filename)
+    img = tf.image.decode_png(file_contents)
+    return img, label
+
+
+def input_pipeline(filenames, batch_size, num_epochs=None):
+    filename_queue = tf.train.string_input_producer(
+        filenames, num_epochs=num_epochs, shuffle=True)
+
+    img, label = read_my_file_format(filename_queue)
+
+    img_batch, label_batch = tf.train.shuffle_batch(
+        [img, label], batch_size=batch_size)
+    return img_batch, label_batch
